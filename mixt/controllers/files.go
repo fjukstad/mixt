8cd7113ed1fcd8724423e3345d70faf76528c153
Bjørn Fjukstad <bjorn@cs.uit.no>
2015-06-23 17:42:41 +0000

    fetching files from opencpu

---
+++ mixt/controllers/files.go
@@ -0,0 +1,22 @@
+package controllers
+
+import (
+	"fmt"
+	"net/http"
+
+	"bitbucket.org/vdumeaux/mixt/mixt/mixt"
+	"github.com/gorilla/mux"
+)
+
+func FileHandler(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	key := vars["key"]
+	filetype := vars["filetype"]
+	//name := vars["name"]
+	body, err := mixt.Get(key, filetype)
+	if err != nil {
+		fmt.Println("Could not read all", err)
+		return
+	}
+	w.Write(body)
+}

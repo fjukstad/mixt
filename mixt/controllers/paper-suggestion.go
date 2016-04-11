f5def79a06b36635768bb15647229c619e43c1cb
Bjørn Fjukstad <bjorn@cs.uit.no>
2016-04-11 12:27:49 +0000

    add paper suggestion ctrllr

---
+++ mixt/controllers/paper-suggestion.go
@@ -0,0 +1,59 @@
+package controllers
+
+import (
+	"fmt"
+	"net/http"
+	"text/template"
+
+	"bitbucket.org/vdumeaux/mixt/mixt/mixt"
+
+	"github.com/fjukstad/kvik/eutils"
+	"github.com/gorilla/mux"
+)
+
+var paperSuggestionTemplate = template.Must(template.ParseFiles("views/base.html", "views/header.html", "views/outside-navbar.html",
+	"views/paper-suggestion.html", "views/footer.html"))
+
+type Results struct {
+	Papers []eutils.DocumentSummary
+}
+
+func PaperSuggestionHandler(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	tissue := vars["tissue"]
+	module := vars["module"]
+
+	m, err := mixt.GetModule(module, tissue)
+	if err != nil {
+		fmt.Println("Could not get module")
+		http.Error(w, err.Error(), 503)
+		return
+	}
+
+	genelist := []string{}
+	for _, gene := range m.Genes {
+		genelist = append(genelist, gene.Name)
+	}
+
+	genelist = genelist[0:10]
+
+	fmt.Println(genelist, module, tissue)
+
+	searchres, err := eutils.Search(genelist)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	//.PmcRefCount .ViewCount
+
+	fmt.Println("SEARCHRES:", searchres)
+	res, err := eutils.Summary(searchres)
+
+	for _, paper := range res.DocumentSummarySet.DocumentSummary {
+		fmt.Println(paper)
+	}
+
+	paperSuggestionTemplate.Execute(w, Results{res.DocumentSummarySet.DocumentSummary})
+
+}

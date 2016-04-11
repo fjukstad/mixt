636e0789d428b881e8bc9e0291ed476c3da4f422
Bjørn Fjukstad <bjorn@cs.uit.no>
2016-04-11 12:38:51 +0000

    mv web app

--- mixt/rstudio/deps.r
+++
@@ -1,6 +0,0 @@
-packages <- c("ggplot2","Hmisc, Rcpp, roxygen2") 
-install.packages(packages, repos='http://cran.us.r-project.org')
-
-source("http://bioconductor.org/biocLite.R")
-pkgs <- c("Biobase" ,"DBI" ,"RSQLite" ,"AnnotationDbi" ,"GO.db" ,"RColorBrewer" ,"latticeExtra" ,"colorspace" ,"munsell", "plyr" ,"scales" ,"reshape" ,"preprocessCore" ,"WGCNA" ,"illuminaHumanv3.db" ,"illuminaHumanv4.db" ,"animation" ,"limma")
-biocLite(pkgs, ask=FALSE) 
